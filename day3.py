8bdf5682d8afe801946dbe7ca05ed1a300965b53
harpreettcodes <harpreettsingh0211@gmail.com>
2026-07-08 22:26:47 +0530

    Added voting eligibility checker

---
+++ day3.py
@@ -0,0 +1,17 @@
+print("---------------VOTING ELIGIBLITY CHECKER-------------------")
+age = int(input("Enter your age:"))
+if age>=18:
+ print("You are Eligible")
+ voter_id= input("Do you have a voter id card? (YES/NO)").lower()
+ if voter_id=="yes":
+  print("Great ! you can vote")
+ elif voter_id=="no":
+  print("Please apply for a voter ID first")
+ else:
+  print("Invalid input please type yes or no")
+else:
+ print("Sorry,you're not eligible")
+ print("You need to need wait",18-age,"more years to vote" )
+print()
+print("THANK FOR USING MY PROGRAM")
+print("------------------------------------------------------------")
