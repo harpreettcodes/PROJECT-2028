623fe945d882593447388865e4d2f93ca7716e6a
harpreettcodes <harpreettsingh0211@gmail.com>
2026-07-09 10:28:06 +0530

    Improved Day 3 voting checker

--- day3.py
+++ day3.py
@@ -1,17 +1,18 @@
-print("---------------VOTING ELIGIBLITY CHECKER-------------------")
+print("-------------------VOTING ELIGIBLITY CHECKER-------------------")
 age = int(input("Enter your age:"))
-if age>=18:
+if age >= 18:
  print("You are Eligible")
- voter_id= input("Do you have a voter id card? (YES/NO)").lower()
+ voter_id=input("Do you have voter ID card? (YES/NO)").lower()
+ while voter_id != "yes" and voter_id != "no" :
+  print("Invalid input. Please enter yes or no only")
+  voter_id=input("Do you have a voter id card ? (YES/NO)").lower()
  if voter_id=="yes":
-  print("Great ! you can vote")
+   print("Great! you can vote")
  elif voter_id=="no":
-  print("Please apply for a voter ID first")
- else:
-  print("Invalid input please type yes or no")
+   print("Please apply for a voter ID first")
 else:
- print("Sorry,you're not eligible")
- print("You need to need wait",18-age,"more years to vote" )
+ print("sorry,you're not eligible")
+ print("You need to need wait", 18 - age,"more years to vote" )
 print()
 print("THANK FOR USING MY PROGRAM")
-print("------------------------------------------------------------")
+print("----------------------------------------------------------------")
