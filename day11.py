13a352be27db2dd776bfdf69307aec16debc1d4f
harpreettcodes <harpreettsingh0211@gmail.com>
2026-07-17 20:38:47 +0530

    Day11: List basics

---
+++ day11.py
@@ -0,0 +1,33 @@
+#List creation
+print("-------------------List creation--------------------")
+data1 = [10, "Hello", True, 3.14]
+print(data1)
+print("----------------------------------------------------")
+#Value add in list
+print("---------------Value add in list--------------------")
+marks = [80, 75, 90, 65] 
+marks[1]=85 
+print(marks)
+print("----------------------------------------------------")
+#append
+print("----------------------Input-------------------------")
+colors=["Red", "Blue", "Green"]
+colors.append("Green")
+print(colors)  
+#append
+print("----------------------Append------------------------")
+marks=[80,75,90,95,100]                                                                                                                                         
+marks[1]=85
+print(marks)  
+print("----------------------------------------------------") 
+#input append
+print("------------------Input append----------------------")
+name1=input("Enter first name: ")
+name2=input('Enter second name:')
+name3=input("Enter third name: ")
+names=[]
+names.append(name1)
+names.append(name2)
+names.append(name3)
+print(names)
+print("----------------------------------------------------") 
