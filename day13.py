c2ccd705dde2be258ccf1a043b7e2e30569e8fce
harpreettcodes <harpreettsingh0211@gmail.com>
2026-07-20 21:24:00 +0530

    Day 14: Learn Python dictionary basics

---
+++ day13.py
@@ -0,0 +1,28 @@
+#Dictionary creation
+print("-------------------Dictionary creation---------------------")
+student = {"name": "Harpreet","age": 20,"course": "BCA"}
+print(student["name"])
+print(student["age"])
+print(student["course"])
+print("-----------------------------------------------------------")
+#Accessing values
+print("-------------------Accessing values------------------------")
+phone = {"brand":"mi","model":"latest","price":100000}
+print(phone["brand"])
+print(phone["model"])
+print(phone["price"])
+print("-----------------------------------------------------------")
+#Updating values
+print("--------------------Updating values------------------------")
+car = {"brand": "Toyota","color": "White"}
+car["color"]="Black"
+print(car["color"])
+print("-----------------------------------------------------------")
+#Adding new keys
+print("--------------------Adding new keys------------------------")
+student = {"name": "Harpreet","age": 20}
+student["course"] = "BCA"
+print(student["name"])
+print(student["age"])
+print(student["course"])
+print("-----------------------------------------------------------")
