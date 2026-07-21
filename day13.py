bd5795b73151ea539f1f83656fe54fc61311e450
harpreettcodes <harpreettsingh0211@gmail.com>
2026-07-21 20:32:07 +0530

    Day 13 with dictionary methods

--- day13.py
+++ day13.py
@@ -26,3 +26,18 @@ print(student["name"])
 print(student["age"])
 print(student["course"])
 print("-----------------------------------------------------------")
+#Dictionary keys
+print("--------------------Dictionary keys------------------------")
+student = {"name": "Harpreet","age": 20,"course": "BCA"}
+print(student.keys())
+print("-----------------------------------------------------------")
+#Dictionary values
+print("--------------------Dictionary values----------------------")
+student = {"name": "Harpreet","age": 20,"course": "BCA"}
+print(student.values())
+print("-----------------------------------------------------------")
+#Dictionary item
+print("----------------------Dictionary item----------------------")
+student = {"name": "Harpreet","age": 20,"course": "BCA"}
+print(student.items())
+print("-----------------------------------------------------------")
