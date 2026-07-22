636e5ebf78f9dab8ff8334ca377dafb39bb36094
harpreettcodes <harpreettsingh0211@gmail.com>
2026-07-22 23:02:55 +0530

    Improve student display formatting

--- day14_student_management.py
+++ day14_student_management.py
@@ -2,24 +2,38 @@ students = []
 print("===============================================")
 print("           Student Management System           ")
 print("===============================================")
-print("1. Add Student")
-print("2. View Stuudents")
-print("3. Exit")
-choice = int(input("Enter your choice: "))
-if choice ==1:
+
+while True:
+   print("1. Add Student")
+   print("2. View Stuudents")
+   print("3. Exit")
+   choice=int(input("Enter 1 to add student,2 to view all students,3 to Exit: "))
+   if choice ==1:
     students_name=input("Enter Studeent Name: ")
     students_age=int(input("Enter Studeent Age: "))
-    print(students_name)
-    print(students_age)
     student = {"name":students_name,"age": students_age} 
     students.append(student)
     print("Student added successfully")  
-elif choice ==2:
+   elif choice ==2:
+     print("=========Students=========")
+     
+     if not students:
+       print("No Students Found.")
+       print("Please add a student first.")
+     else:
+      for student in students:
+       print("Name:",student["name"])
+       print("Age: ",student["age"]) 
+       print("--------------------------")
+     print("==========================") 
+   elif choice ==3:
     print()
-elif choice ==3:
-    print()   
-else:
-    print("Invalid input")
+    break 
+   else:
+    print(" Invalid Input! ")
 print("===============================================")
 print("          Thanks For Using This Program        ")
 print("===============================================")
+
+
+   
