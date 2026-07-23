ac0d8c9b52dc744bf4db433a8f21695505be3c5f
harpreettcodes <harpreettsingh0211@gmail.com>
2026-07-23 16:21:38 +0530

    Add student search feature

--- day14_student_management.py
+++ day14_student_management.py
@@ -6,8 +6,9 @@ print("===============================================")
 while True:
    print("1. Add Student")
    print("2. View Stuudents")
-   print("3. Exit")
-   choice=int(input("Enter 1 to add student,2 to view all students,3 to Exit: "))
+   print("3. Search Student")
+   print("4. Exit")
+   choice=int(input("Enter 1 to add student,2 to view all students,3 To search a student,4 To exit: "))
    if choice ==1:
     students_name=input("Enter Studeent Name: ")
     students_age=int(input("Enter Studeent Age: "))
@@ -24,9 +25,24 @@ while True:
       for student in students:
        print("Name:",student["name"])
        print("Age: ",student["age"]) 
-       print("--------------------------")
-     print("==========================") 
+       print("--------------------------") 
    elif choice ==3:
+       found = False
+       search_student=input("Enter student name: ")
+       
+       for student in students:
+        if search_student == student["name"]:
+         found = True
+         print("Student Found")
+         print("--------------------------") 
+         print("Name:",student["name"])
+         print("Age: ",student["age"]) 
+         print("--------------------------")
+         break 
+
+       if found == False:
+        print("Student Not Found")
+   elif choice ==4:
     print()
     break 
    else:
