e7fac4b9e6da8e9a1494808b3f493a98ade504f9
harpreettcodes <harpreettsingh0211@gmail.com>
2026-07-25 22:16:27 +0530

    Add update student feature

--- day14_student_management.py
+++ day14_student_management.py
@@ -7,11 +7,12 @@ while True:
    print("1. Add Student")
    print("2. View Stuudents")
    print("3. Search Student")
-   print("4. Exit")
-   choice=int(input("Enter 1 to add student,2 to view all students,3 To search a student,4 To exit: "))
+   print("4. Update Student")
+   print("5. Exit")
+   choice=int(input("Enter 1 to add student,2 to view all students,3 To search a student,4 To update student name or age: ,5 To exit: "))
    if choice ==1:
-    students_name=input("Enter Studeent Name: ")
-    students_age=int(input("Enter Studeent Age: "))
+    students_name=input("Enter Student Name: ").strip()
+    students_age=int(input("Enter Student Age: "))
     student = {"name":students_name,"age": students_age} 
     students.append(student)
     print("Student added successfully")  
@@ -28,7 +29,7 @@ while True:
        print("--------------------------") 
    elif choice ==3:
        found = False
-       search_student=input("Enter student name: ")
+       search_student=input("Enter student name: ").strip()
        
        for student in students:
         if search_student == student["name"]:
@@ -39,10 +40,45 @@ while True:
          print("Age: ",student["age"]) 
          print("--------------------------")
          break 
+    
+       if found == False:
+        print("Student Not Found")
+   elif choice ==4:  
+       if not students:
+              print("No Students Found.")
+              print("Please add a student first.")
+              continue
+       found = False
+       inp=input("Enter student name you want to update: ").strip()
+       for student in students:
+        if inp == student["name"]:
+         found = True
+         print("1. Update Name")
+         print("2. Update Age")
+         print("3. Update Both")
+         choice2=int(input("Please Select choose one from above: "))
+         if choice2 == 1:
+          new_name=input("Enter new name: ").strip()
+          student["name"]= new_name
+          print("Student name updated succesfully!")
+          break
+         elif choice2 ==2:
+          new_age=int(input("Enter new age: "))
+          student["age"]= new_age
+          print("Student age updated succesfully!")
+          break
+         elif choice2 ==3:
+          new_name=input("Enter new name: ").strip()
+          new_age=int(input("Enter new age: "))
+          student["name"]= new_name
+          student["age"]= new_age
+          print("Student name updated succesfully!")
+          print("Student age updated succesfully!")
+          break
 
        if found == False:
         print("Student Not Found")
-   elif choice ==4:
+   elif choice ==5:
     print()
     break 
    else:
@@ -50,6 +86,3 @@ while True:
 print("===============================================")
 print("          Thanks For Using This Program        ")
 print("===============================================")
-
-
-   
