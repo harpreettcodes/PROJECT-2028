c4e2d6e47c7a6807b87a0d6ac2652fafcecd5ad8
harpreettcodes <harpreettsingh0211@gmail.com>
2026-08-03 13:38:13 +0530

    Day 15: Learn OOP basics with classes, objects, attributes and constructors

---
+++ day18_oop.py
@@ -0,0 +1,31 @@
+#Python OOP
+print("----------------------Without INIT---------------------")
+class Student:
+ pass
+student1 = Student()
+student2 = Student()
+student1.name = "Harpreet"
+student1.age = 20
+student2.name ="Rahul"
+student2.age = 18
+print(student1.name, student1.age)
+print(student2.name, student2.age)
+print("-------------------------------------------------------")
+#__init__ 
+print("-----------------------With INIT-----------------------")
+class Student:
+ def __init__ (self,name,age):
+  self.name = name 
+  self.age = age
+student1 = Student("Harpreet",20)
+print(student1.name)
+print(student1.age)
+print("-------------------------------------------------------")
+class car:
+ def __init__(self,brand,color,year):
+  self.brand = brand
+  self.color = color
+  self.year = year
+car1 = car("BMW","Black",2028)
+print(car1.brand,car1.color,car1.year)
+print("-------------------------------------------------------")
